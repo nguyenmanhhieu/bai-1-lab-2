5565ff5a40fdcca488bb00acd0cc642f0db9ee94
nguyenmanhhieu <hieunm14092001@gmail.com>
2022-06-05 16:16:55 +0700

    bai 1 lab 2
    nguyen manh hieu

---
+++ nguyen manh hieu b1.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  code test ++i
+//
+//  Created by Hiếu Hee on 04/06/2022.
+//
+
+#include <stdio.h>
+int main()
+{
+int num = 10;
+char ch = 'A';
+float f = 25.3;
+double d = 25.3;
+printf("num = %d\n", num);
+printf("ch = %c\n", ch);
+printf("f = %.2f\n", f);
+printf("d = %f\n", d);
+return 0;
+}
+
